e653fb9d90c2f788f130ea5468da2fbf8cad2236
unknown <thaedeveloper@gmail.com>
2022-08-25 19:31:22 -0500

    reto01 completado de retosdeprogamacion.com de MoureDev

--- reto01.js
+++ reto01.js
@@ -1,22 +1,16 @@
 /*
- * Escribe un programa que muestre por consola (con un print) los
- * números de 1 a 100 (ambos incluidos y con un salto de línea entre
- * cada impresión), sustituyendo los siguientes:
- * - Múltiplos de 3 por la palabra "fizz".
- * - Múltiplos de 5 por la palabra "buzz".
- * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
  */
-
-for(let i=1; i<=100; i++){ 
-    if(i % 5 === 0 && i % 3 === 0){
-        console.log('fizzbuzz')
-    }else if(i % 3 === 0){
-        console.log('fizz') 
-    }else if(i % 5 === 0){
-        console.log('buzz')
-    }else{
-        console.log(i) 
-    }  
-    
+/* Ejemplo de Anagrama ==>  sergio - riesgo */
+/* Ejemplo de Anagrama ==>  amor - roma */
+function dosPalabras(cadena1, cadena2){
+    if(cadena1.toLowerCase().split('').sort().join('') === cadena2.toLowerCase().split('').sort().join('') ) return console.log(true)
+        return console.log(false)
 }
+dosPalabras('AmOr', 'rOMas')
 
